b66ac6092671b2bc52de164e147dd9133001b2ff
Gustavo Vituri <gustavovituri@gmail.com>
2020-10-07 09:14:39 -0300

    Full Project Upload

---
+++ buscador_de_gifs/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:buscador_de_gifs/ui/home_page.dart';
+import 'package:buscador_de_gifs/ui/gif_page.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(
+        hintColor: Colors.lightBlue,
+        primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.lightBlue)),
+          hintStyle: TextStyle(color: Colors.lightBlue),
+        )),
+  )
+  );
+}
